177f43505e83831dffaeda6a708fe8c6adf367af
Billy Vong <billy@opstarts.com>
2015-11-06 18:46:54 -0800

    Add actual test for button component

--- test/button-test.js
+++ test/button-test.js
@@ -1,11 +1,15 @@
-const chai = require('chai');
 const React = require('react');
-chai.should();
+import {createRenderer} from 'react-addons-test-utils';
+import expect, { createSpy, spyOn, isSpy } from 'expect';
 
 const Button = require('../src/button');
+
 describe('Button', () => {
-  it('should pass tests', () => {
-    var button = <Button />;
-    button.should.exist;
+  it('can render an empty button', () => {
+    const renderer = createRenderer();
+    renderer.render(<Button />);
+    const component = renderer.getRenderOutput();
+
+    expect(component.type).toEqual('button');
   });
 });
